3be3a74ec3067b0b6fd7a12ec10512e02d4e1260
JinMo-source <qkrwslah123@naver.com>
2023-04-15 18:16:32 +0900

    노마드 코더 #3~#4 내용

---
+++ Desktop/wetube/src/controller/userController.js
@@ -0,0 +1,21 @@
+export const join = (req, res) => {
+  return res.send("Join");
+};
+
+export const login = (req, res) => {
+  return res.send("Login");
+};
+export const logout = (req, res) => {
+  return res.send("Logout");
+};
+export const user = (req, res) => {
+  return res.send("user");
+};
+
+export const userEdit = (req, res) => {
+  return res.send("userEdit");
+};
+
+export const profileDelete = (req, res) => {
+  return res.send("profileDelete");
+};
---
+++ Desktop/wetube/src/controller/videoController.js
@@ -0,0 +1,14 @@
+export const search = (req, res) => {
+  return res.send("Search");
+};
+export const see = (req, res) => {
+  {
+    return res.send("see");
+  }
+};
+export const videoEdit = (req, res) => {
+  return res.send("videoEdit");
+};
+export const videoDelete = (req, res) => {
+  return res.send("videoDelete");
+};
