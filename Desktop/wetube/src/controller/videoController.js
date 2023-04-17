a5e4cd78b5ee7ca8667227e4641d80caa65d1d21
JinMo-source <qkrwslah123@naver.com>
2023-04-17 14:09:24 +0900

    watch,upload,edit 완료

--- Desktop/wetube/src/controller/videoController.js
+++ Desktop/wetube/src/controller/videoController.js
@@ -18,25 +18,46 @@ export const see = (req, res) => {
     return res.send("see");
   }
 };
+/CREATE (CRUD)/;
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = async (req, res) => {
   const { title, description, createDate } = req.body;
   try {
-    console.log(Video);
     await Video.create({
       title,
       description,
     });
+    console.log(Video);
     return res.redirect("/");
-  } catch (err) {
+  } catch (error) {
     return res.render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
   }
 };
+/READ (CURD)/;
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: video.title, video });
+};
+
+/UPDATE(CRUD)/;
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  console.log(video);
+  return res.render("edit", { pageTitle: "EDIT", video });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  // console.log(req.body)
+};
+/DELETE (CRUD)/;
 export const videoDelete = (req, res) => {
   return res.send("videoDelete");
 };
--- Desktop/wetube/src/router/videoRouter.js
+++ Desktop/wetube/src/router/videoRouter.js
@@ -1,15 +1,18 @@
 import express from "express";
 import {
-  see,
   getUpload,
   postUpload,
+  watch,
+  getEdit,
+  postEdit,
   videoDelete,
 } from "../controller/videoController.js";
 
 const videoRouter = express.Router();
 
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-videoRouter.get(`/:id([0-9a-f]{24})`, see);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id([0-9a-f]{24})/delete", videoDelete);
 
 export default videoRouter;
